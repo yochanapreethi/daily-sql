bbb6b305e590e2f3652ccbe8ab087b1049e44433
Gorusu Yochana Preethi <yochanapreethi2807@gmail.com>
2025-08-16 12:41:49 +0530

    Update day-08.sql

--- day-08.sql
+++ day-08.sql
@@ -22,3 +22,27 @@ SELECT (
 * `LIMIT 1 OFFSET 1` → skip the highest, take the 2nd
 * If no 2nd salary exists → returns `NULL`'''
 
+
+
+'''## Q-02: Delete all duplicate emails, keeping only the one with the **smallest id**.
+
+Table: Person
+| id | email                                       |
+| -- | ------------------------------------------- |
+| 1  | [john@example.com](mailto:john@example.com) |
+| 2  | [bob@example.com](mailto:bob@example.com)   |
+| 3  | [john@example.com](mailto:john@example.com) |'''
+
+--code
+DELETE p1
+FROM Person p1
+JOIN Person p2
+  ON p1.email = p2.email
+ AND p1.id > p2.id;
+
+'''## Explanation
+
+* `JOIN` matches duplicate emails in the same table
+* `p1.id > p2.id` → keeps the smallest id deletes the rest
+* So only one unique email with the lowest id remains'''
+
