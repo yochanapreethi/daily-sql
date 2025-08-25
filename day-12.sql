8b1503d1f1761a30fb3b1da30bac165f27efd922
Gorusu Yochana Preethi <yochanapreethi2807@gmail.com>
2025-08-25 22:31:07 +0530

    Update day-12.sql

--- day-12.sql
+++ day-12.sql
@@ -8,10 +8,10 @@ Q-01: We need to obtain a histogram of tweets posted per user in the year 2022.
 Tweets Table  
 | tweet_id | user_id | msg                                               | tweet_date          |
 |----------|---------|---------------------------------------------------|---------------------|
-| 214252   | 111     | Am considering taking Tesla private at $420        | 2021-12-30 00:00:00 |
-| 739252   | 111     | Despite the constant negative press covfefe        | 2022-01-01 00:00:00 |
-| 846402   | 111     | Following @NickSinghTech changed my life!          | 2022-02-14 00:00:00 |
-| 241425   | 254     | If the salary is so competitive, why hide it?      | 2022-03-01 00:00:00 |
+| 214252   | 111     | Am considering taking Tesla private at $420       | 2021-12-30 00:00:00 |
+| 739252   | 111     | Despite the constant negative press covfefe       | 2022-01-01 00:00:00 |
+| 846402   | 111     | Following @NickSinghTech changed my life!         | 2022-02-14 00:00:00 |
+| 241425   | 254     | If the salary is so competitive, why hide it?     | 2022-03-01 00:00:00 |
 | 231574   | 148     | I no longer have a manager. I cant be managed     | 2022-03-23 00:00:00 |
 
 Output  
